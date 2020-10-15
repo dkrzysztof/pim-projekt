137e33a57abde85ee093ca3a35e1928936014252
Jakub Kalina <kalinakuba6@gmail.com>
2020-10-15 10:44:08 +0200

    Added controllers, basic folder structure

---
+++ server/server/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost("login")]
+        public Task<IActionResult> Login()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("register")]
+        public Task<IActionResult> Register()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("logout")]
+        public Task<IActionResult> Logout()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("update")]
+        public Task<IActionResult> UpdateUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("delete-account")]
+        public Task<IActionResult> DeleteAccount()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ server/server/Controllers/NoteController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NoteController : ControllerBase
+    {
+        [HttpGet]
+        public Task<IActionResult> GetNotes()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("create")]
+        public Task<IActionResult> CreateNote()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("update")]
+        public Task<IActionResult> UpdateNote()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{noteId}")]
+        public Task<IActionResult> DeleteNote()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
